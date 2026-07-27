1db336cb956b363603651de4733ab2a161db9d72
cornking58-blip <cornking58@gmail.com>
2026-07-28 00:32:21 +0300

    Simplify ambient background to thin-line minimalism

--- frontend/src/components/AmbientBackground.tsx
+++ frontend/src/components/AmbientBackground.tsx
@@ -5,100 +5,56 @@ import { colors } from '../theme/colors';
 export function AmbientBackground() {
   return (
     <View pointerEvents="none" style={StyleSheet.absoluteFill}>
-      <View style={styles.deepShade} />
-      <View style={styles.orbTop} />
-      <View style={styles.orbSide} />
-      <View style={styles.orbBottom} />
+      <View style={styles.base} />
       <View style={styles.ringTop} />
-      <View style={styles.ringSide} />
+      <View style={styles.ringBottom} />
       <View style={styles.lineOne} />
       <View style={styles.lineTwo} />
-      <View style={styles.diamond} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  deepShade: {
+  base: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: colors.background,
   },
-  orbTop: {
-    position: 'absolute',
-    width: 360,
-    height: 360,
-    borderRadius: 180,
-    backgroundColor: colors.primary,
-    opacity: 0.11,
-    top: -235,
-    right: -125,
-  },
-  orbSide: {
-    position: 'absolute',
-    width: 260,
-    height: 260,
-    borderRadius: 130,
-    backgroundColor: colors.cyan,
-    opacity: 0.045,
-    top: 280,
-    left: -190,
-  },
-  orbBottom: {
-    position: 'absolute',
-    width: 320,
-    height: 320,
-    borderRadius: 160,
-    backgroundColor: colors.primaryBright,
-    opacity: 0.045,
-    bottom: -230,
-    right: -130,
-  },
   ringTop: {
     position: 'absolute',
-    width: 205,
-    height: 205,
-    borderRadius: 103,
+    width: 240,
+    height: 240,
+    borderRadius: 120,
     borderWidth: 1,
     borderColor: colors.pattern,
-    top: -70,
-    right: -48,
+    top: -135,
+    right: -95,
   },
-  ringSide: {
+  ringBottom: {
     position: 'absolute',
-    width: 150,
-    height: 150,
-    borderRadius: 75,
+    width: 180,
+    height: 180,
+    borderRadius: 90,
     borderWidth: 1,
     borderColor: colors.pattern,
-    top: 410,
-    left: -82,
+    bottom: -115,
+    left: -90,
   },
   lineOne: {
     position: 'absolute',
     width: 260,
     height: 1,
     backgroundColor: colors.pattern,
-    top: 215,
-    right: -75,
-    transform: [{ rotate: '-32deg' }],
+    top: 205,
+    right: -95,
+    transform: [{ rotate: '-28deg' }],
   },
   lineTwo: {
     position: 'absolute',
     width: 220,
     height: 1,
     backgroundColor: colors.pattern,
-    bottom: 170,
-    left: -70,
-    transform: [{ rotate: '28deg' }],
-  },
-  diamond: {
-    position: 'absolute',
-    width: 72,
-    height: 72,
-    borderWidth: 1,
-    borderColor: colors.pattern,
-    top: '42%',
-    right: -38,
-    transform: [{ rotate: '45deg' }],
+    bottom: 165,
+    left: -85,
+    transform: [{ rotate: '24deg' }],
   },
 });
